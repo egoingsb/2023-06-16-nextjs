db12ca178b6a86b35d9f0707649bdded259c6861
egoing <egoing@gmail.com>
2023-06-16 16:09:55 +0900

    상세 보기 기능 구현

--- app/read/[id]/page.tsx
+++ app/read/[id]/page.tsx
@@ -3,6 +3,15 @@ type ReadProps = {
     id: string;
   };
 };
-export default function Read(props: ReadProps) {
-  return <>Read!!! {props.params.id}</>;
+export default async function Read(props: ReadProps) {
+  const resp = await fetch(
+    "http://localhost:3000/api/topics/" + props.params.id
+  );
+  const topic = await resp.json();
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}
+    </>
+  );
 }
