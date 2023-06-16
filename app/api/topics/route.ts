ae3c5d2ea0ba24c2c7c1fada1e09b04532cb9572
egoing <egoing@gmail.com>
2023-06-16 14:47:06 +0900

    데이터 추가 기능 구현

--- app/api/topics/route.ts
+++ app/api/topics/route.ts
@@ -3,3 +3,11 @@ import { db } from "./db";
 export async function GET() {
   return NextResponse.json(db.topics);
 }
+
+let nextId = 3;
+export async function POST(request: Request) {
+  const res = await request.json();
+  const newTopic = { id: nextId++, title: res.title, body: res.body };
+  db.topics.push(newTopic);
+  return NextResponse.json(newTopic);
+}
